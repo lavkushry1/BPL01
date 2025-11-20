e0b3ac1f1b0480e1377c500e6070f7997536be8c
lavkushry1 <lavkushry@gmail.com>
2025-11-21 00:52:07 +0530

    refactor: Replace direct axios import with defaultApiClient and introduce response unwrapping utility.

--- eventia-ticketing-flow1/src/services/api/userApi.ts
+++ eventia-ticketing-flow1/src/services/api/userApi.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { API_BASE_URL } from './apiUtils';
+import { defaultApiClient } from './apiUtils';
+import { unwrapApiResponse } from './responseUtils';
 
 // Define interfaces for user data
 export interface User {
